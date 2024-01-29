65567507ae2a6b4b3f9f69c370b291dc4fff0978
IogaMaster <iogamastercode@gmail.com>
2024-01-29 11:35:05 -0700

    add rust-analyzer to flake.nix

--- flake.nix
+++ flake.nix
@@ -5,20 +5,22 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = {
-    self,
-    naersk,
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachSystem flake-utils.lib.allSystems (system: let
+  outputs =
+    { self
+    , naersk
+    , nixpkgs
+    , flake-utils
+    , ...
+    }:
+    flake-utils.lib.eachSystem flake-utils.lib.allSystems (system:
+    let
       pkgs = (import nixpkgs) {
         inherit system;
       };
 
-      naersk' = pkgs.callPackage naersk {};
-    in {
+      naersk' = pkgs.callPackage naersk { };
+    in
+    {
       defaultPackage = self.packages.${system}.manix;
       packages.manix = naersk'.buildPackage {
         src = ./.;
@@ -26,7 +28,7 @@
 
       # For `nix develop` (optional, can be skipped):
       devShell = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [rustc cargo];
+        nativeBuildInputs = with pkgs; [ rustc cargo rust-analyzer ];
       };
     });
 }
